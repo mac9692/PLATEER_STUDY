5c6d14a925245ff779939656f2af264b43215e6a
leekihoon <53985471+lee-kihoon@users.noreply.github.com>
2022-02-25 00:30:41 +0900

    https://www.acmicpc.net/problem/14681

--- LeeKiHoon/20220224.java
+++ LeeKiHoon/20220224.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 class Main {
     
     public static void main(String[] args) {
-        
+         
         Scanner sc = new Scanner(System.in);
         
         int x = sc.nextInt();
