0d170d4c4a933f2ffa6e9cdcaed4fa5879d0c9b9
dengjuyu <86100708+dengjuyu@users.noreply.github.com>
2022-03-11 01:37:19 +0900

    Create 20220310.cpp
    codeup 1026

---
+++ juhyunyu/20220310.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int h, m, s;
+	cin >> h;
+	cin.ignore(256, ':');
+	cin >> m;
+	cin.ignore(256, ':');
+	cin >> s;
+	cout << m << endl;
+
+}
