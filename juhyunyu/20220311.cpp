d2e3d6928e19880d45080087232befb1fa8e4c27
dengjuyu <86100708+dengjuyu@users.noreply.github.com>
2022-03-12 01:30:38 +0900

    Create 20220311.cpp
    codeup 1028, 1029, 1030, 1031, 1032, 1033, 1034, 1035, 1036, 1037, 1038, 1039, 1040

---
+++ juhyunyu/20220311.cpp
@@ -0,0 +1,155 @@
+// 1028
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	unsigned int n;
+	cin >> n;
+	cout << n << endl;
+}
+// 1029
+/*
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+    double n;
+	cin >> n;
+	printf("%.11f", n);
+}
+*/
+// 1030
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	long long int n;
+	cin >> n;
+	cout << n << endl;
+}
+*/
+// 1031
+/*
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	printf("%o", n);
+}
+*/
+// 1032
+/*
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	printf("%x", n);
+}
+*/
+// 1033
+/*
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	printf("%X", n);
+}
+*/
+// 1034
+/*
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int n;
+	scanf("%o", &n);
+	printf("%d", n);
+}
+*/
+// 1035
+/*
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int n;
+	scanf("%x", &n);
+	printf("%o", n);
+}
+*/
+// 1036
+/*
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	char w;
+	scanf("%c", &w);
+	printf("%d", w);
+}
+*/
+// 1037
+/*
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	printf("%c", n);
+}
+*/
+// 1038
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	long long int a, b;
+	cin >> a >> b;
+	cout << a + b << endl;
+}
+*/
+// 1039
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	long long int a, b;
+	cin >> a >> b;
+	cout << a + b << endl;
+}
+*/
+// 1040
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a;
+	cin >> a;
+	cout << -a << endl;
+}
+*/
