d15f48b0b6adc06988adeee91ae5d9f8889dd68b
dengjuyu <86100708+dengjuyu@users.noreply.github.com>
2022-03-13 02:42:45 +0900

    Create 20220312.cpp
    codeup 1041 ~ 1050

---
+++ juhyunyu/20220312.cpp
@@ -0,0 +1,135 @@
+// 1041
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	char a;
+	scanf("%c", &a);
+	printf("%c", a+1);
+	
+}
+// 1042
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << a / b << endl;
+	
+}
+*/
+// 1043
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << a % b << endl;
+	
+}
+*/
+// 1044
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	long long int a;
+	cin >> a;
+	cout << ++a << endl;
+	
+}
+*/
+// 1045
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	long long int a;
+	int b;
+	cin >> a >> b;
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << a % b << endl;
+	cout << fixed;
+	cout.precision(2);
+	cout << (float)a / (float)b << endl;
+	
+}
+*/
+// 1046
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << a + b + c << endl;
+	cout << fixed;
+	cout.precision(1);
+	cout << (float)(a + b + c) / 3.0 << endl;
+}
+*/
+// 1047
+/*
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int a;
+	scanf("%d", &a);
+	printf("%d", a << 1);
+}
+*/
+// 1048
+/*
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	scanf_s("%d %d", &a, &b);
+	printf("%d", a << b);
+}
+*/
+// 1049
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	bool result = a > b ? 1 : 0;
+	cout << result << endl;
+}
+*/
+// 1050
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	bool result = a == b ? 1 : 0;
+	cout << result << endl;
+}
+*/
