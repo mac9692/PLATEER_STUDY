d395d99af664be2ede68c9239c140d46f8dcb072
dengjuyu <86100708+dengjuyu@users.noreply.github.com>
2022-03-14 23:47:37 +0900

    Create 20220314.cpp
    codeup 1051~1060

---
+++ juhyunyu/20220314.cpp
@@ -0,0 +1,127 @@
+// 1051
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	bool result = a <= b ? 1 : 0;
+	cout << result << endl;
+}
+// 1052
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	bool result = a != b ? 1 : 0;
+	cout << result << endl;
+}
+*/
+// 1053
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a;
+	cin >> a;
+	cout << !a << endl;
+}
+*/
+// 1054
+/*
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	printf("%d", a && b);
+}
+*/
+// 1055
+/*
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	printf("%d", a || b);
+}
+*/
+// 1056
+/*
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	printf("%d", (a&&!b)||(!a&&b));
+}
+*/
+// 1057
+/*
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	printf("%d", (a&&b)||(!a&&!b));
+}
+*/
+// 1058
+/*
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	printf("%d", !a&&!b);
+}
+*/
+// 1059
+/*
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int a;
+	cin >> a;
+	printf("%d", ~a);
+}
+*/
+// 1060
+/*
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	printf("%d", (a&b));
+}
+*/
